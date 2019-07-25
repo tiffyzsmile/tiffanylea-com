fe1f0dccb7cf4eeaf19f91c072f891567cb343f3
tiffany may <tiffyzsmile@gmail.com>
2019-07-25 16:55:20 -0700

    Added prettier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./Home";
-import Route404 from "./Route404";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Route404 from './Route404';
 import './App.css';
 
 function App() {
   return (
-      <Router>
-          <Route exact path="/" component={Home} />
-          <Route component={Route404} />
-      </Router>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route component={Route404} />
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function Home() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                    This should be test.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. This should be test.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default Home;
-
