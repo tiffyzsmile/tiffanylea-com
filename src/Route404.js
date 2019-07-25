a72047d3302d5e39e69fcda461fdda4c1ad5ae9a
tiffany may <tiffyzsmile@gmail.com>
2019-07-25 16:41:19 -0700

    Added react-router

---
+++ src/Route404.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    Link,
+} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+
+function Route404() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Page Not Found
+                </p>
+                <Link to="/">Home</Link>
+            </header>
+
+        </div>
+    );
+}
+
+export default Route404;
+
