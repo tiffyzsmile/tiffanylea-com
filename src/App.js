22e31aad329ef17e37e9f14b4d86e298273f851c
tiffany may <tiffyzsmile@gmail.com>
2019-07-25 16:41:19 -0700

    Added react-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import Route404 from "./Route404";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This should be test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Route exact path="/" component={Home} />
+          <Route component={Route404} />
+      </Router>
   );
 }
 
