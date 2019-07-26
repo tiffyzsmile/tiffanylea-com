9a7a7c17a1caf35a598243394b09e940e7aeba13
tiffany may <tiffyzsmile@gmail.com>
2019-07-25 19:14:02 -0700

    Moved babel and eslint configs out of package.json, enabled hot-reloading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Route404 from './Route404';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Route exact path="/" component={Home} />
       <Route component={Route404} />
     </Router>
   );
-}
+};
 
-export default App;
+export default hot(module)(App);
--- src/Route404.js
+++ src/Route404.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 
 function Route404() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo.svg" className="App-logo" alt="logo" />
         <p>Page Not Found</p>
         <Link to="/">Home</Link>
       </header>
