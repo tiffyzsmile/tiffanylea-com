35f13da9b62e1222aa66d92e53e6fc7001a59e99
tiffany may <tiffyzsmile@gmail.com>
2019-12-16 11:13:47 -0800

    Updated tag categories

--- src/components/CategoryField/index.js
+++ src/components/CategoryField/index.js
@@ -6,23 +6,9 @@ const CategoryField = () => {
     <label htmlFor="category">
       Category
       <Field id="category" name="category" component="select">
-        <optgroup label="Industries">
-          <option>Real Estate</option>
-          <option>E-Commerce</option>
-          <option>Higher Ed</option>
-          <option>Manufacturing</option>
-          <option>Automotive</option>
-          <option>Attorneys/Law Firms</option>
-          <option>Financial</option>
-          <option>Medical</option>
-          <option>Utilities</option>
-          <option>Tourism</option>
-          <option>Religious</option>
-          <option>Global</option>
-          <option>Non-profit</option>
-        </optgroup>
-        <option>Languages</option>
-        <option>Project Types</option>
+        <option>Industry</option>
+        <option>Language</option>
+        <option>Project Type</option>
         <option>User Interface (UI)</option>
         <option>User Experience (UX)</option>
         <option>Data</option>
@@ -33,9 +19,9 @@ const CategoryField = () => {
         <option>Tooling</option>
         <option>AWS</option>
         <option>Devops</option>
-        <option>Roles</option>
-        <option>Libraries</option>
-        <option>Frameworks</option>
+        <option>Role</option>
+        <option>Library</option>
+        <option>Framework</option>
         <option>Software</option>
       </Field>
     </label>
